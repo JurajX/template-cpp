73bea4dec4a60d6becc19b7be502d1b3bf469740
Juraj <37395233+JurajX@users.noreply.github.com>
2021-12-21 14:48:36 +0100

    add docs

--- include/my_lib/my_lib.hpp
+++ include/my_lib/my_lib.hpp
@@ -3,7 +3,19 @@
 
 namespace my_lib {
 
+/**
+ * @brief Print a butified message to a console output.
+ *
+ * @param message The message to butify and print.
+ */
 auto printNiceMessage(std::string_view message) -> void;
+
+/**
+ * @brief Butify a given message.
+ *
+ * @param message The message to butify.
+ * @return std::string The budified message.
+ */
 auto makeNiceMessage(std::string_view message) -> std::string;
 
 }    // namespace my_lib
