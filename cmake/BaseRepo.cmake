a50fa44320845a11b35462e7f788c7648cdb5abe
Juraj <37395233+JurajX@users.noreply.github.com>
2022-02-20 07:45:38 +0100

    use chached downloads

--- cmake/BaseRepo.cmake
+++ cmake/BaseRepo.cmake
@@ -1,7 +1,8 @@
 include(FetchContent)
 FetchContent_Declare(
     BaseRepo
-    GIT_REPOSITORY https://github.com/JurajX/BaseRepo.git
-    GIT_TAG        master
+    GIT_REPOSITORY      https://github.com/JurajX/BaseRepo.git
+    GIT_TAG             master
+    UPDATE_DISCONNECTED ON
 )
 FetchContent_MakeAvailable(BaseRepo)
