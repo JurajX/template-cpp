61f6df5e42972d44a3a2455d1c5113e5b1f5e288
Juraj <37395233+JurajX@users.noreply.github.com>
2022-02-20 08:05:10 +0100

    update fetch content

--- cmake/BaseRepo.cmake
+++ cmake/BaseRepo.cmake
@@ -2,7 +2,8 @@ include(FetchContent)
 FetchContent_Declare(
     BaseRepo
     GIT_REPOSITORY      https://github.com/JurajX/BaseRepo.git
-    GIT_TAG             master
+    GIT_TAG             origin/master
+    GIT_PROGRESS        ON
     UPDATE_DISCONNECTED ON
 )
 FetchContent_MakeAvailable(BaseRepo)
